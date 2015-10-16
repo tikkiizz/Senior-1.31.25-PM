e41719e3a719088e54b0335ab64283ab03cec037
tikkiizz <tikkiizz@hotmail.com>
2015-10-16 18:52:46 +0700

    Initial Commit

---
+++ Senior/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Senior
+//
+//  Created by CPE KMUTT on 10/16/2558 BE.
+//  Copyright © 2558 Naphat Jindathanakit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
